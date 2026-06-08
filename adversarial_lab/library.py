17593533f3b1354ed32c30a0cf580f0647ed8b65
Stefano Magnolfi <stefano.magnolfi1@edu.unifi.it>
2026-06-08 19:26:24 +0200

    FEAT: implemented (not working) JARN

--- adversarial_lab/library.py
+++ adversarial_lab/library.py
@@ -1,5 +1,7 @@
 import torch
 import torch.nn.functional as F
+import torch.nn as nn
+from numpy.random import uniform
 
 def train_loop(model, dataloader, optimizer, criterion, device, adv=False, attack_params_dict=None):
     model.train()
@@ -36,20 +38,38 @@ def train_loop(model, dataloader, optimizer, criterion, device, adv=False, attac
     accuracy = correct / num_samples
     return total_loss / num_samples, accuracy
 
-def eval_loop(model, dataloader, criterion, device):
+def eval_loop(model, dataloader, criterion, device, adv=False, attack_params_dict = None):
     model.eval()
     total_loss = 0.0
     correct = 0
     with torch.no_grad():
         for inputs, labels in dataloader:
             inputs, labels = inputs.to(device), labels.to(device)
+
+            if adv and attack_params_dict is not None:
+                adv_images = generate_adversarial(
+                    model=model, 
+                    criterion=criterion, 
+                    images=inputs, 
+                    labels=labels, 
+                    eps=attack_params_dict['eps'], 
+                    target_class=attack_params_dict['target_class'], 
+                    num_iterations=attack_params_dict['num_iterations'],
+                    device=device
+                )
+                inputs = torch.cat([inputs, adv_images], dim=0)
+                labels = torch.cat([labels, labels], dim=0)
+
+            model.train() # generate_adversarial sets the model to eval mode
             outputs = model(inputs)
             loss = criterion(outputs, labels)
             total_loss += loss.item() * inputs.size(0)
             _, predicted = torch.max(outputs, 1)
             correct += (predicted == labels).sum().item()
-    accuracy = correct / len(dataloader.dataset)
-    return total_loss / len(dataloader.dataset), accuracy
+    
+    num_samples = len(dataloader.dataset) * (2 if adv else 1)
+    accuracy = correct / num_samples
+    return total_loss / num_samples, accuracy
 
 def test_loop(model, dataloader, device, adv=False, criterion=torch.nn.CrossEntropyLoss(), attack_params_dict=None):
     
@@ -100,6 +120,7 @@ def get_msp_scores(model, dataloader, device):
             
     return scores
 
+@torch.enable_grad()
 def generate_adversarial(model, criterion, images, labels, eps, target_class=None, num_iterations=1, device='cpu'):
     model.eval()  
     model =model.to(device)
@@ -151,3 +172,110 @@ def generate_adversarial(model, criterion, images, labels, eps, target_class=Non
             perturbed_images = torch.clamp(perturbed_images, min_val, max_val)
 
     return perturbed_images.detach()
+
+def JARN_train_loop(model, adaptor, discriminator, model_optimizer, adaptor_optimizer, discriminator_optimizer, dataloader, device, eps, discriminator_interval=20):
+
+    model.train()
+    adaptor.train()
+    discriminator.train()
+    
+    lambda_adv = 1
+
+    cross_entropy_loss = nn.CrossEntropyLoss()
+    BCE_loss = nn.BCEWithLogitsLoss()
+
+    total_g_loss = 0.0
+    total_adv_loss = 0.0
+    correct=0
+
+    adv_updates = 0
+
+    for batch_idx, (images, labels) in enumerate(dataloader):
+
+        images,labels = images.to(device), labels.to(device)
+
+        noise = torch.empty_like(images).uniform_(-eps, eps)
+        images = images + noise
+
+        images.requires_grad = True
+
+        # Compute first part of L_cls
+        output = model(images)
+        cls_loss = cross_entropy_loss(output, labels)
+        
+        # Retrieve Jacobian
+        jacobian = torch.autograd.grad(
+            outputs=cls_loss, 
+            inputs=images, 
+            create_graph=True, # Crucial for double-backprop
+            retain_graph=True
+        )[0]
+        
+        J_prime = adaptor(jacobian)
+
+        real_labels = torch.full((images.shape[0], 1), 0.9).to(device)
+
+        # Compute L_adv and train Discriminator
+        if (batch_idx+1) % discriminator_interval == 0:
+            discriminator_optimizer.zero_grad()
+
+            out_real = discriminator(images)
+            loss_real = BCE_loss(out_real, real_labels)
+
+            fake_labels = torch.zeros(images.shape[0], 1).to(device)
+            out_fake = discriminator(J_prime.detach()) # Detached to not influence gradient of adaptor
+            loss_fake = BCE_loss(out_fake, fake_labels)
+
+            adv_loss = loss_real + loss_fake  # NOTE: sign of this adv_loss is reversed from the L_adv of the paper
+            adv_loss.backward()
+
+            torch.nn.utils.clip_grad_norm_(discriminator.parameters(), max_norm=1.0)
+            total_adv_loss += adv_loss.item()
+            adv_updates += 1
+            discriminator_optimizer.step()
+
+        # Train Model and Adaptor
+
+        ''' How I would have done it
+        adaptor_optimizer.zero_grad()
+
+        fake_labels = torch.zeros(images.shape[0], 1).to(device)
+        out_fake = discriminator(J_prime)
+        loss_fake = BCE_loss(out_fake, fake_labels)
+
+        adv_loss = loss_real + loss_fake
+        adv_loss.backward()
+        adaptor_optimizer.step() # with negative learning rate
+
+        model_optimizer.zero_grad()
+        final_cls_loss = cls_loss + (lambda_adv*adv_loss)
+        final_cls_loss.backward()
+        model_optimizer.step()
+        '''
+
+        model_optimizer.zero_grad()
+        adaptor_optimizer.zero_grad()
+
+        # With Gemini advise
+        out_fake_G = discriminator(J_prime) # f_disc(J')
+        g_adv_loss = BCE_loss(out_fake_G, real_labels) # BCE(p,y) = -[ylog(p) + (1-y)log(1-p)] => BCE(p,1) = -log(p), in this case -log(f_disc(J'))  
+
+        # We minimize -log(f_disc(J')) instead of maximizing log(1-f_disc(J')) to avoid vanishing gradient ("Non-Saturating" trick)
+
+        g_loss = cls_loss + (lambda_adv * g_adv_loss)
+        total_g_loss += g_loss.item() * images.size(0)
+        
+        g_loss.backward() # Computes gradients for model and adaptor, cls_loss is independent from adaptor parameters
+        
+        model_optimizer.step() 
+        adaptor_optimizer.step() 
+
+        _, predicted = torch.max(output, 1)
+        correct += (predicted == labels).sum().item()
+
+    accuracy = correct / len(dataloader.dataset)
+    total_g_loss = total_g_loss/len(dataloader.dataset)
+    avg_adv_loss = total_adv_loss / adv_updates if adv_updates > 0 else 0.0
+
+    return total_g_loss, total_adv_loss, accuracy
+        
